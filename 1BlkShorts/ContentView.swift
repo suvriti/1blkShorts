590f19288500169cb14ccd770fb29fe5004c703e
babbar01 <amanbabber01@gmail.com>
2023-03-18 00:04:54 +0530

    Initial Commit

---
+++ 1BlkShorts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  1BlkShorts
+//
+//  Created by Aman Babbar on 18/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1BlkShorts/_BlkShortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _BlkShortsApp.swift
+//  1BlkShorts
+//
+//  Created by Aman Babbar on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct _BlkShortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
