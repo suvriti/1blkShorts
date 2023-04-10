e22616ec07b15ee591a7b469467855ac259ae5bb
Navin Kumar Singh <navinkrsingh@gmail.com>
2023-04-10 15:22:36 +0530

    added few working playlist urls

--- 1BlkShorts/ContentView.swift
+++ 1BlkShorts/ContentView.swift
@@ -16,18 +16,18 @@ struct ContentView: View {
     @State var selectedTab = 0
     
     var videoPlayer = [
-        AVPlayer(url: URL(string: "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3")!),
-        AVPlayer(url: URL(string: "https://hack2023ms-inct.streaming.media.azure.net/acaa9bfa-76e9-4f0d-a2d4-c3f674aea59e/monkey-clip.ism/manifest(format=m3u8-cmaf)")!),
-        AVPlayer(url: URL(string: "https://hack2023ms-inct.streaming.media.azure.net/451cca72-73c0-4c88-a868-f47eaaa4e043/newYorkFlip-clip.ism/manifest(format=m3u8-cmaf)")!),
-        AVPlayer(url: URL(string: "https://hack2023ms-inct.streaming.media.azure.net/acaa9bfa-76e9-4f0d-a2d4-c3f674aea59e/monkey-clip.ism/manifest(format=m3u8-cmaf)")!),
-        AVPlayer(url: URL(string: "https://hack2023ms-inct.streaming.media.azure.net/451cca72-73c0-4c88-a868-f47eaaa4e043/newYorkFlip-clip.ism/manifest(format=m3u8-cmaf)")!)
+         AVPlayer(url: URL(string: "https://hack2023ms-inct.streaming.media.azure.net/451cca72-73c0-4c88-a868-f47eaaa4e043/newYorkFlip-clip.ism/manifest(format=m3u8-cmaf)")!),
+         AVPlayer(url: URL(string: "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8")!),
+         AVPlayer(url: URL(string: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8")!),
+         AVPlayer(url: URL(string: "http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_stereo_subs.m3u8")!),
+         AVPlayer(url: URL(string: "https://hack2023ms-inct.streaming.media.azure.net/451cca72-73c0-4c88-a868-f47eaaa4e043/newYorkFlip-clip.ism/manifest(format=m3u8-cmaf)")!)
     ]
     
     func scrollingReelView() -> some View{
         return NavigationView{
             GeometryReader{geometry in
                 TabView(selection: $selectedTab){
-                    ForEach(0..<5){i in
+                    ForEach(0..<videoPlayer.count, id: \.self){i in
                         ReelView(player: videoPlayer[i],
                                  reelIndex: i,
                         selectedTabItem: $selectedTab)
